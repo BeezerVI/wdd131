0d9cfd3a0b066b29c08d1afb968bcfd3192fc293
BeezerVI <hyrummckay@gmail.com>
2024-06-15 15:50:02 -0600

    updated Last Modified

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,10 +2,10 @@ const currentYear = new Date().getFullYear();
 const currentMonth = new Date().getMonth();
 const currentDay = new Date().getDate();
 
-const lastModified = document.lastModified;
+let lastModified = Date(document.lastModified);
 
 const footer = document.querySelector('footer');
-
+//${currentDay}/${currentMonth}/${currentYear}`;
 const copyrightParagraph = document.createElement('p');
-copyrightParagraph.textContent = ` ${currentDay}/${currentMonth}/${currentYear}`;
+copyrightParagraph.textContent = `Last Modified : ${lastModified}`;
 footer.appendChild(copyrightParagraph);
