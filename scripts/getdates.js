42b58c726b585846232694e9cea49f04b4e81e98
BeezerVI <hyrummckay@gmail.com>
2024-06-15 15:42:55 -0600

    did stuuff

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,9 +1,11 @@
 const currentYear = new Date().getFullYear();
+const currentMonth = new Date().getMonth();
+const currentDay = new Date().getDate();
 
 const lastModified = document.lastModified;
 
 const footer = document.querySelector('footer');
 
 const copyrightParagraph = document.createElement('p');
-copyrightParagraph.textContent = `©️ ${currentYear}`;
+copyrightParagraph.textContent = ` ${currentDay}/${currentMonth}/${currentYear}`;
 footer.appendChild(copyrightParagraph);
